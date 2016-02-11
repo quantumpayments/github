6349cd3ba72b3d9d2b76d246ebe13a91c42d7901
Melvin Carvalho <melvincarvalho@gmail.com>
2016-02-11 11:20:43 +0100

    Add tests

--- test/github.js
+++ test/github.js
@@ -1,38 +1,24 @@
-var github = require("../lib/github");
+var parser = require("../lib/github");
 
 /* test/my_test.js */
 var expect = require('chai').expect;
 
 describe('Github Functions', function () {
 
-  describe('initGithub', function() {
-    it('initGithub is a function', function () {
-      expect( (github.initGithub)).to.be.a('function');
+  describe('userToWebID', function() {
+    it('userToWebID is a function', function () {
+      expect( (parser.userToWebID)).to.be.a('function');
+    });
+    it('userToWebID gives the right output for github username', function () {
+      expect(parser.userToWebID('test')).to.equal('https://github.com/test#this');
+    });
+    it('userToWebID gives the right output for email', function () {
+      expect(parser.userToWebID('andrei@fcns.eu')).to.equal('mailto:andrei@fcns.eu');
+    });
+    it('userToWebID gives the right output for github http', function () {
+      expect(parser.userToWebID('https://github.com/user#this')).to.equal('https://github.com/user#this');
     });
   });
 
-  describe('resultsToWebcredits', function() {
-    it('resultsToWebcredits is a function', function () {
-      expect( (github.resultsToWebcredits)).to.be.a('function');
-    });
-  });
-
-  describe('getAllResults', function() {
-    it('getAllResults is a function', function () {
-      expect( (github.getAllResults)).to.be.a('function');
-    });
-  });
-
-  describe('printResults', function() {
-    it('printResults is a function', function () {
-      expect( (github.printResults)).to.be.a('function');
-    });
-  });
-
-  describe('compactResults', function() {
-    it('compactResults is a function', function () {
-      expect( (github.compactResults)).to.be.a('function');
-    });
-  });
 
 });
